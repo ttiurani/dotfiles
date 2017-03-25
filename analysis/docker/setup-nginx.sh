42c9cf3a3281df94aa0a86eb8b4180c416f87d5d
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-03-25 12:50:27 +0200

    More systematic ports

--- analysis/docker/setup-nginx.sh
+++ analysis/docker/setup-nginx.sh
@@ -4,5 +4,5 @@
 docker stop nginx && docker rm nginx
 
 # Run Nginx with this command
-docker run -p 9001:80 --name nginx -v /home/ttiurani/dotfiles/analysis/docker/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/ttiurani/devel/em/extendedmind:/extendedmind:ro -d nginx:alpine
+docker run -p 9001:9001 --name nginx -v /home/ttiurani/dotfiles/analysis/docker/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/ttiurani/devel/em/extendedmind:/extendedmind:ro -d nginx:alpine
 
