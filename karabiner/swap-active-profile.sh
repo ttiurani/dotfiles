6691ef749d063b99610491ffec4429165b7e5824
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-12-22 10:07:26 +0200

    Migrate to codeberg.org

--- karabiner/swap-active-profile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bashe
-sed -i '' 's/"selected": false/"selected": TEMP/g' /Users/ttiurani/.config/karabiner/karabiner.json 
-sed -i '' 's/"selected": true/"selected": false/g' /Users/ttiurani/.config/karabiner/karabiner.json 
-sed -i '' 's/"selected": TEMP/"selected": true/g' /Users/ttiurani/.config/karabiner/karabiner.json 
